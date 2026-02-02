ef3d3a1cafcbe2d4c5ad51e2c2e2fc0a1a4017a3
mohammed rayaan pasha <mdrayaanpasha@gmail.com>
2026-02-02 13:49:00 +0530

    more test try:2

--- K8s/index.js
+++ K8s/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.json({ status: 'ok', service: 'pull kubectl changes', test:"101" });
+  res.json({ status: 'ok', service: 'pull kubectl changes v2', test:"102" });
 });
 
 const PORT = process.env.PORT || 3000;
